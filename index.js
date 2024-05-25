f74762568243c7b01d965dd28334ce643fe2380d
Taki <danielbinsemp@gmail.com>
2024-05-24 21:08:11 -0500

    Desafio feito

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('ola mundo muahhhhhhh')
+console.log('ola mundo')
+let nome = "Alucio"
+let xp = 30010
+if (xp<1000){
+    console.log(`O heroi ${nome} tá no nivel Ferro`)
+}else if(xp>=1000&&xp<2000){
+    console.log(`O heroi ${nome} tá no nivel Bronze`)
+}else if(xp>=2000&&xp<5000){
+    console.log(`O heroi ${nome} tá no nivel Prata`)
+}else if(xp>=5000&&xp<7000){
+    console.log(`O heroi ${nome} tá no nivel Ouro`)
+}else if(xp>=7000&&xp<8000){
+    console.log(`O heroi ${nome} tá no nivel Platina`)
+}else if(xp>=8000&&xp<9000){
+    console.log(`O heroi ${nome} tá no nivel Ascendente`)
+}else if(xp>=9000&&xp<10000){
+    console.log(`O heroi ${nome} tá no nivel imortal`)
+}else if(xp>=10000){
+    console.log(`O heroi ${nome} tá no nivel Radiante`)
+}
